c18cae196a20021f413dfbdada117e6976133d15
Louis-Guillaume DUBOIS <contact@lgdubois.fr>
2020-03-23 20:52:55 +0100

    Utilisation de l'interface commune

---
+++ printemps/env.js
@@ -0,0 +1,54 @@
+const sites = [
+    {
+        title: 'Bible de Sacy',
+        url: 'https://bible.sacy.be'
+    },
+    {
+        title: 'Heures de Port-Royal',
+        url: 'https://heures.sacy.be'
+    },
+    {
+        title: 'Bible de Royaumont',
+        url: 'https://royaumont.sacy.be'
+    },
+    {
+        title: 'Hiver - Bréviaire Le Tourneux',
+        url: 'https://breviaire.sacy.be/hiver'
+    },
+    {
+        title: 'Printemps - Bréviaire Le Tourneux',
+        url: 'https://breviaire.sacy.be/printemps'
+    },
+    {
+        title: 'Été - Bréviaire Le Tourneux',
+        url: 'https://breviaire.sacy.be/ete'
+    },
+    {
+        title: 'Automne - Bréviaire Le Tourneux',
+        url: 'https://breviaire.sacy.be/automne'
+    }
+]
+
+const UI_ENV = {
+    title: 'Printemps—Bréviaire de Nicolas Le Tourneux',
+    canonical: 'https://breviaire.sacy.be/printemps',
+    dataBasePath:'',
+    uiPath: '../ui',
+    bookSelectorLabel: 'Part.',
+    chapterSelectorLabel: 'À :',
+    sites,
+    links: [
+        {
+            title: 'rapporter une erreur',
+            url: 'https://github.com/breviaire-pr/breviaire-pr.github.io/issues'
+        },
+        {
+            title: 'code source du site web',
+            url: 'https://github.com/breviaire-pr/breviaire-pr.github.io'
+        },
+        {
+            title: 'autres œuvres de Port-Royal',
+            url: 'https://www.sacy.be'
+        }
+    ]
+}
